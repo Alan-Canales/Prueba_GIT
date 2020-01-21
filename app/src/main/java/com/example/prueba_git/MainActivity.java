ff57a28df3fd36154e60e65254eb4a120cde28b4
Alan-Canales <60125325+Alan-Canales@users.noreply.github.com>
2020-01-20 23:11:56 -0600

    Nuevo comentario

--- app/src/main/java/com/example/prueba_git/MainActivity.java
+++ app/src/main/java/com/example/prueba_git/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         /*Hola JOna ahuevo que funciono*/
+        /*Aqui si agarra**/
     }
 }
